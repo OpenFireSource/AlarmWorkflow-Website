23fffa8e46fbfb5344db4d4099f2d31533e75573
chrishensel <futurefwarrior@googlemail.com>
2015-04-19 09:52:43 +0200

    Several build script fixes
    - Do not require modifying PATH variable anymore
    - Also added script for building RELEASE

--- BuildWebsite.Release.bat
+++ BuildWebsite.Release.bat
@@ -1,6 +1,6 @@
 @echo off
 rem -------------------------------------------------
-rem AlarmWorkflow build script (DEBUG)
+rem AlarmWorkflow build script (RELEASE)
 rem 
 rem Please change "Framework64" to "Framework" in the following path
 rem if you're running this script on a x86 or 32-bit OS.
@@ -9,6 +9,6 @@ SET build=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe
 
 echo -------------------------------------------------
 echo Build Website(s)...
-%build% Website\Website.sln /p:Configuration=Debug /verbosity:minimal
+%build% Website\Website.sln /p:Configuration=Release /verbosity:minimal
 
 pause
